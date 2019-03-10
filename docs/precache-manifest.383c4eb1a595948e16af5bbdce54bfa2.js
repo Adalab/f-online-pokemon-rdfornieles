4063bb8d8949788f6306becd3af1d49b415f02c8
rdfornieles <rdfornieles@gmail.com>
2019-03-10 23:07:49 +0100

    pages

---
+++ docs/precache-manifest.383c4eb1a595948e16af5bbdce54bfa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddd5084dff8d7606019645f62c1712c6",
+    "url": "./static/media/PokemonLogo.ddd5084d.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "b9123ab04fce9bcf74b1",
+    "url": "./static/js/main.0502c781.chunk.js"
+  },
+  {
+    "revision": "5ffb0bb1f882bef602f4",
+    "url": "./static/js/2.2c77fd6b.chunk.js"
+  },
+  {
+    "revision": "b9123ab04fce9bcf74b1",
+    "url": "./static/css/main.9e6187c7.chunk.css"
+  },
+  {
+    "revision": "38f078e6e29109790685029bdd77c181",
+    "url": "./index.html"
+  }
+];
